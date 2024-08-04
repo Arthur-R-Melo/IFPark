235ed7629354311cb16cb9a615c1b5be8c741a18
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2024-08-04 19:20:37 -0300

    Update login.js

--- web/javascript/login.js
+++ web/javascript/login.js
@@ -4,21 +4,31 @@ function enviar() {
     try {
         let email = document.getElementById("email").value;
         let password = document.getElementById("senha").value;
+        if (validaEmail(email)) {
+            request = new XMLHttpRequest();
+            /*Alterei o evento pois com o readystatechange seria necessário verificar se o estado da requisição está como finalizado.
+            Utilizando os eventos load e error creio que o código fica,além de tudo, mais legível*/
+            request.addEventListener("load", processaDadoServidor, false);
+            request.onerror = function () {
+                alert(`ERROR: ${request.status}`);
+            }
+            request.open('GET', `http://4.228.227.52:8080/atividadelogin/autenticar?email=${email}&senha=${password}`
 
-        request = new XMLHttpRequest();
-        //Dá pra fazer usando o evento load/método onload mas precisa tratar erro com onerror
-        request.addEventListener("load", processaDadoServidor, false);
-        request.onerror = function() {
-            alert(`ERROR: ${request.status}`);
+                , true);
+            request.send(null);
+        } else {
+            alert("Email inválido");
+            return;
         }
-        request.open('GET', `http://4.228.227.52:8080/atividadelogin/autenticar?email=${email}&senha=${password}`
-
-            , true);
-        request.send(null);
 
     } catch (exception) {
         alert('problema no envio de dados');
     }
+}
+
+function validaEmail(email) {
+    const regex = /^[^\s]+@[^\s]+\.[^\s]+$/;
+    return regex.test(email);
 }
 
 function processaDadoServidor() {
