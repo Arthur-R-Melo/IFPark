04626090bebea736c17e518ee89ed3ba66aca035
Valter Simão <103610549+valtersimao@users.noreply.github.com>
2024-08-12 16:07:56 -0300

    a

--- yplate/__init__.py
+++ yplate/__init__.py
@@ -1,3 +1,14 @@
 ## Plate-Scan ---------
 
 ## Detect car/vehicle Number plates easily
+
+from commands import detect
+from ops import model_config,display_top,load_model
+cfg,weights,classes = load_model()
+
+img = "images/car.jpg"
+hide_img = False
+save_img = True
+hide_out = False			
+## Detect
+detect(img,cfg,weights,classes,save_img,hide_img,hide_out)
