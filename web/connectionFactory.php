b64a83b9405f9f44fbaa0ab46e8c985703d7426b
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2024-09-11 18:49:57 -0300

    Create connectionFactory.php

---
+++ web/connectionFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+function getConnection()
+{
+    $config = include('infoDB.php');
+    $host = $config['host'];
+    $username = $config['username'];
+    $password = $config['password'];
+    $database = $config['dbname'];
+
+    $conn = new mysqli($host, $username, $password, $database);
+    if ($conn->connect_error) {
+        die("Erro na conexão: " . $conn->connect_error);
+    }
+}
