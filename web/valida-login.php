575af7cfbece762e0422adef7999b0cbb021d82a
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2024-10-21 11:34:11 -0300

    Update valida-login.php
    Melhorando a legibilidade do código

--- web/valida-login.php
+++ web/valida-login.php
@@ -1,5 +1,6 @@
 <html>
-    TODO
+TODO
+
 </html>
 
 <?php
@@ -8,77 +9,78 @@ session_set_cookie_params(['httponly' => true]);
 session_start();
 
 session_regenerate_id(true);
+
+$_SESSION['logged_in'] = false;
+
 include('connectionFactory.php');
 
-if (!isset($_POST['email']) || !isset($_POST['senha'])) {
+if (!isset($_POST['email']) || !isset($_POST['senha']) || empty($_POST['email']) || empty($_GET['senha'])) {//Caso verdadeiro o código volta a página ou morre depois do if
 ?>
     <script>
         alert("Email ou senha não foram informados!")
         window.history.back();
     </script>
     <?php
-    die;
-}
-
-if (empty($_POST['email']) || empty($_GET['senha'])) {
-    ?>
-    <script>
-        alert("Email ou senha não foram informados!")
-        window.history.back();
-    </script>
-    <?php
-    die;
+    die();
 }
 
 try {
 
-$conn = getConnection();
-$user = $_POST['email'];
-$password = $_POST['senha'];
+    $conn = getConnection();
+    $user = $_POST['email'];
+    $password = $_POST['senha'];
 
-$sql = 'SELECT * FROM Administrador WHERE user = ?';
-$stmt = $conn->prepare($sql);
-$stmt->bind_param('s', $user);
-$stmt->execute();
-$resultado = $stmt->get_result();
+    $sql = 'SELECT * FROM Administrador WHERE user = ?';
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $user);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
 
 
-    if ($resultado->num_rows > 0) {
-        $rows = $resultado->fetch_assoc();
-        if ($rows['senha'] === $password) {
-            $_SESSION['logged_in'] = true;
-            $_SESSION['ID_adm'] = $resultado['ID'];
-            $_SESSION['nome'] = $resultado['nome']
+    if ($resultado->num_rows <= 0) {//Caso verdadeiro o código volta a página ou morre depois do if
     ?>
-            <script>
-                alert("Funcionou")
-                console.log("Funcionou")
-            </script>
-        <?php
-        
-        } else {
-            $_SESSION['logged_in'] = false;
-        ?>
-            <script>
-                alert("Senha errada")
-                console.log("Senha errada")
-            </script>
-        <?php
-        }
-    } else {
-        $_SESSION['logged_in'] = false;
-        ?>
         <script>
             alert("Usuário não existe")
             console.log("Usuário não existe")
+            window.history.back();
         </script>
-<?php
+    <?php
+    die();
+    }
+
+    $rows = $resultado->fetch_assoc();
+
+    if ($rows['senha'] === $password) {
+
+        $_SESSION['logged_in'] = true;
+        $_SESSION['ID_adm'] = $resultado['ID'];
+        $_SESSION['nome'] = $resultado['nome']
+    ?>
+        <script>
+            alert("Funcionou")
+            console.log("Funcionou")
+            window.history.back();
+        </script>
+    <?php
+
+    } else {//Caso verdadeiro o código volta a página ou morre depois do else
+    ?>
+        <script>
+            alert("Senha errada")
+            console.log("Senha errada")
+            window.history.back();
+        </script>
+    <?php
+    die();
     }
 } catch (Exception $e) {
-    $_SESSION['logged_in'] = false;
     ?>
-    <script>console.log(<?php addslashes($e)?>)</script>
-    <?php
+    <script>
+        alert("Ocorreu uma exceção!!")
+        console.log(<?php addslashes($e) ?>)
+        window.history.back();
+    </script>
+<?php
 }
 
 
