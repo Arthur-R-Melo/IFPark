0c01105f2692d07dc324b4ab617bc987e9eef2a3
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2025-01-07 14:54:37 -0300

    Create cadastrar-adm.php

---
+++ web/cadastrar-adm.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IFPark - Cadastro de Administrador</title>
+    <link rel="icon" href="images/logo_ifpark_icon.png" type="image/png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <img src="images/ifpark_logoBranco_TXT.png" alt="IfPark Logo">
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="index.html">INÍCIO</a></li>
+            <li><a href="contato.html">CONTATO</a></li>
+        </ul>
+    </nav>
+
+    <div class="container" id="form-container">
+        <h1 id="h1-login">Cadastro de Administrador</h1>
+        <br>
+        <form action="" onsubmit="" method="POST">
+            
+        </form>
+    </div>
+</body>
+
+</html>
