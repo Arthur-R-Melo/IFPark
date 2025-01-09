3f54df3fc6627e6af56de4c3479b30da185ce320
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2025-01-09 14:54:48 -0300

    Update cadastrar-adm.php

--- web/cadastrar-adm.php
+++ web/cadastrar-adm.php
@@ -1,3 +1,14 @@
+<?php
+if (!(isset($_SESSION['instituicao']) && $_SESSION['instituicao'] === true)) {
+?>
+    <script>
+        alert("Você precisa estar logado como uma instituição para isso!");
+        window.history.back();
+    </script>
+<?php
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -26,10 +37,45 @@
     <div class="container" id="form-container">
         <h1 id="h1-login">Cadastro de Administrador</h1>
         <br>
-        <form action="" onsubmit="" method="POST">
-            
+        <form action="cadastrar-adm.php" onsubmit="return validarFormulario(event)" method="POST">
+            <div class="mb-3">
+                <label for="nome" class="form-label">Nome da conta</label>
+                <input type="text" name="nome" id="nome" class="form-control" placeholder="Insira o nome do administrador">
+                <div class="invalid-feedback" id="erroNome">
+                    O nome da conta deve estar preenchido!
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="user" class="form-label">Nome de Usuário</label>
+                <input type="text" name="user" id="user" class="form-control" placeholder="Insira o nome de usuário">
+                <div class="invalid-feedback" id="erroUser">
+                    O nome de usuário deve estar preenchido!
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="senha" class="form-label">Nome da conta</label>
+                <input type="password" name="senha" id="senha" class="form-control" placeholder="Insira a senha do administrador">
+                <div class="invalid-feedback" id="erroSenha">
+                    O nome da conta deve estar preenchido!
+                </div>
+            </div>
         </form>
     </div>
 </body>
+<script>
+    function validarFormulario(event) {
+        let temErro = false
+
+        //Limpar erros anteriores
+        const campos = ['nome', 'user', 'senha'];
+        campos.forEach(campo => {
+            const input = document.getElementById(campo);
+            const erroDiv = document.getElementById('erro' + campo.charAt(0).toUpperCase() + campo.slice(1));
+
+            input.classList.remove('is-invalid');
+            erroDiv.style.display = 'none';
+        });
+    }
+</script>
 
 </html>
