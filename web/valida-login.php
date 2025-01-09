13210b823287364ea23930996f6cded2b1aa238e
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2025-01-09 14:48:52 -0300

    Update valida-login.php

--- web/valida-login.php
+++ web/valida-login.php
@@ -63,6 +63,7 @@ try {
         $_SESSION['email'] = $rows['email'];
 
         if ($_POST['administrador'] == 'adm') {
+            $_SESSION['instituicao'] == false;
             $_SESSION['ID'] = $rows['ID'];
             $_SESSION['instituicao_ID'] = $rows['instituicao'];
             $_SESSION['nivelAcesso'] = $rows['nivelDeAcesso'];
@@ -74,6 +75,7 @@ try {
             $rows = $resultado->fetch_assoc();
             $_SESSION['instituicaoNome'] = $rows['nome'];
         }else {
+            $_SESSION['instituicao'] == true;
             $_SESSION['instituicao_ID'] = $rows['ID'];
             $_SESSION['documento'] = $rows['doc'];
             $_SESSION['instituicaoNome'] = $rows['nome'];
