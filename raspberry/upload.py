9e73144ed4cfebb57c5b03478b414c7d32d8bebc
Valter Simão <103610549+valtersimao@users.noreply.github.com>
2025-01-10 10:35:31 -0300

    Update upload.py

--- raspberry/upload.py
+++ raspberry/upload.py
@@ -2,7 +2,7 @@ import requests
 
 def enviarFoto():
     #url = 'https://52.233.90.226:5010/carDetection'
-    url = 'http://localhost:5010/carDetection'
+    url = 'https://52.233.90.226:5010/carDetection'
     file_path = 'teste.jpg'
 
     with open(file_path, 'rb') as file:
@@ -10,5 +10,3 @@ def enviarFoto():
         response = requests.post(url, files=files)
 
     print(response.text)
-
-enviarFoto()
