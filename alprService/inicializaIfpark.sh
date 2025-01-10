1de2be6ec5e8d318deb76c828597c23b09630f55
Valter Simão <103610549+valtersimao@users.noreply.github.com>
2025-01-10 09:18:09 -0300

    Update inicializaIfpark.sh

--- alprService/inicializaIfpark.sh
+++ alprService/inicializaIfpark.sh
@@ -1,2 +1,2 @@
-nohup /home/ifpark/5000/venv/bin/python main.
-nohup /home/ifpark/5010/venv/bin/python recievePhoto.py
+nohup /home/ifpark/5000/venv/bin/python 5000/main.py
+nohup /home/ifpark/5010/venv/bin/python 5010/main.py
