d87c529c1cf79970b336077ce979569c0c0b0f5d
Arthur-R-Melo <105597771+Arthur-R-Melo@users.noreply.github.com>
2025-01-21 15:05:33 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- web/control/excluir-adm.php
+++ web/control/excluir-adm.php
@@ -2,7 +2,7 @@
 include_once "connectionFactory.php";
 session_start();
 
-if (!isset($_GET['idAdm'])) {
+if (!isset($_GET['id'])) {
 ?>
 
     <script>
@@ -18,47 +18,66 @@ if (!isset($_SESSION['instituicao']) || $_SESSION['instituicao'] !== true || !is
         alert("Você precisa estar logado como uma instituição para isso!");
         window.history.back();
     </script>
-<?php
+    <?php
     die;
 }
 
-$id = intval($_GET['idAdm']);
-$idInst = $_SESSION['instituicao_ID'];
-$conn = getConnection();
+try {
+    $id = intval($_GET['id']);
+    $idInst = $_SESSION['instituicao_ID'];
+    $conn = getConnection();
 
-$sqlCheck = "SELECT id FROM Administrador WHERE id = ? AND instituica0 = ?";
-$stmtCheck = $conn->prepare($sql);
-$stmtCheck->bind_param("ii", $id, $idInst);
-$stmtCheck->execute();
-$result = $stmtCheck->get_result();
+    $sqlCheck = "SELECT * FROM Administrador WHERE id = ?";
+    $stmtCheck = $conn->prepare($sqlCheck);
+    $stmtCheck->bind_param("i", $id);
+    $stmtCheck->execute();
+    $result = $stmtCheck->get_result();
 
-if ($result->num_rows > 0) {
-    $sqlDelete = "DELETE FROM Administrador WHERE id = ?";
-    $stmtDelete = $conn->prepare($sqlDelete);
-    $stmtDelete->bind_param("i", $id);
-
-    if ($stmtDelete->execute()) {
-        ?>
-        <script>
-            alert("Administrador excluído com sucesso")
-            window.history.back();
-        </script>
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if ($row['instituicao'] !== $idInst) {
+            ?>
+            <script>
+                alert("Você não tem permissão para excluir esse administrador")
+                window.history.back();
+            </script>
         <?php
+        die;
+        }
+        $sqlDelete = "DELETE FROM Administrador WHERE id = ?";
+        $stmtDelete = $conn->prepare($sqlDelete);
+        $stmtDelete->bind_param("i", $id);
+
+        if ($stmtDelete->execute()) {
+    ?>
+            <script>
+                alert("Administrador excluído com sucesso")
+                window.history.back();
+            </script>
+        <?php
+        } else {
+        ?>
+            <script>
+                alert("Erro ao excluir Administrador!")
+                window.history.back();
+            </script>
+        <?php
+        }
     } else {
         ?>
         <script>
-            alert("Erro ao excluir Administrador!")
+            alert("Adm não encontrado!")
             window.history.back();
         </script>
-        <?php
+    <?php
     }
-    
-}else {
+} catch (Exception $e) {
     ?>
-        <script>
-            alert("Você não tem permissão para excluir esse administrador")
-            window.history.back();
-        </script>
-        <?php
+    <script>
+        alert("<?php echo $e->getMessage() ?>")
+    </script>
+<?php
 }
+
+
 ?>
